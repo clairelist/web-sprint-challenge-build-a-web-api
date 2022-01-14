91aaff0b84f2a9a83280a056767b355dff3000a1
Claire List <claire.list2501@gmail.com>
2022-01-14 10:19:14 -0700

    GET actions/id built

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,20 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model');
+
+async function validateActionId(req, res, next) { //asyncronous bc we are calling database ! -- try/catch way
+   try {
+        const action = await Actions.get(req.params.id);
+        if (!action) {
+            res.status(404).json({message: 'action not found !'});
+        } else {
+          req.action = action; //this is how we store it ! Not the other way round claire ;-)
+          next(); //THEN invoke next
+        }
+   } catch(err) {
+    res.status(500).json({message: 'BAD REQUEST MADE! ', error: err});
+   }
+  }
+
+  module.exports = {
+      validateActionId
+  }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,7 +3,9 @@
 //DATA sectionr
 const express = require('express');
 
-// const {} = require('./actions-middleware');
+ const {
+    validateActionId
+} = require('./actions-middlware'); //MIDDLEWARE IS MISSPELLED ON THE FILE ITSELF FFS
 
 const Actions = require('./actions-model');
 
@@ -17,6 +19,10 @@ router.get('/', (req, res) => { //get all, return an array !
     .catch(err=>{
         res.status(404).json(err);
     })
+  });
+
+  router.get('/:id', validateActionId,(req,res)=>{ //get by ID; will require middleware !
+      res.json(req.action);
   });
 
   module.exports = router;
